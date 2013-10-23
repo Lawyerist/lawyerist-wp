65d3f8dc8148c057711d768f695c9fd99bdbe05b
Sam Glover <sam@samglover.net>
2013-10-23 00:45:51 -0500

    Fix sidebar colors and footer text.

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 	<div id="footer_content">
 		<p>Lawyerist is a blog about law practice. We help lawyers find the tools to improve their practices, we lead a conversation about law practice, and we have a strong online community of lawyers, the <a href="http://lab.lawyerist.com">Lawyerist LAB</a>.</p>
 
-		<p>Lawyerist is written by a <a href="http://lawyerist.com/about/">bunch of different people</a>. Some are practicing lawyers, some are former practicing lawyers who are now doing other things related to law practice, and one or two aren't lawyers at all. Because lawyers don't know everything.
+		<p>Lawyerist is written by a bunch of different people. Some are practicing lawyers, some are former practicing lawyers who are now doing other things related to law practice, and one or two aren't lawyers at all. Because lawyers don't know everything.
 
 		<p>Lawyerist.com is designed in-house and built on <a href="http://wordpress.org/" rel="nofollow">WordPress</a>. WordPress is awesome. Headlines are set in <a href="http://www.google.com/fonts/specimen/Roboto+Slab" rel="nofollow">Roboto Slab</a>. Body text is <a href="http://www.google.com/fonts/specimen/Source+Sans+Pro" rel="nofollow">Source Sans Pro</a>. Icons are <a href="http://genericons.com/" rel="nofollow">Genericons</a>.</p>
 	</div>
