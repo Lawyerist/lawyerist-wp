a13873f73657a0426d025a44dedea66f34d9fc50
Sam Glover <sam@samglover.net>
2013-12-06 23:15:57 -0600

    Simplify pagenav.
    Seriously, I made this as complicated as it could possibly be when I
    first did it.

--- single.php
+++ single.php
@@ -17,27 +17,26 @@
 
 			<div class="post">
 		
-				<h1 class="headline remove_bottom" itemprop="headline"><?php the_title(); ?></h1>
+				<h1 class="headline" itemprop="headline"><?php the_title(); ?></h1>
 				
 				<div class="postmeta">
 					<div class="comment_link th_comment_link"><a href="#disqus_thread"><div class="comment_bubble"></div> <?php comments_number('leave a comment','1 comment','% comments'); ?></a></div>
-					<div class="author_link">by <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a> on  on <span itemprop="datePublished"><?php the_time('F jS, Y'); ?></span></div>
+					<div class="author_link">by <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a> on <span itemprop="datePublished"><?php the_time('F jS, Y'); ?></span></div>
 					<div class="clear"></div>
 				</div>
 
-				<div itemprop="image">
-					<?php if ( has_post_thumbnail() && has_tag('big-image') ) {
-						the_post_thumbnail('large');
-					}
+				<?php if ( has_post_thumbnail() && has_tag('big-image') ) { ?>
+					<div itemprop="image"><?php the_post_thumbnail('large'); ?></div>
+				<?php }
 
-					elseif ( has_post_thumbnail() ) {
-						the_post_thumbnail('medium');
-					} ?>
-				</div>
+				elseif ( has_post_thumbnail() ) { ?>
+					<div itemprop="image"><?php the_post_thumbnail('medium'); ?></div>
+				<?php } ?>
 				
 				<div class="post_body" itemprop="articleBody">
 					<?php include('notes.php'); ?>
 					<?php the_content(); ?>
+					<?php wp_link_pages(); ?>
 				</div>
 				
 				<p class="post_bio_sep"></p>
@@ -59,12 +58,13 @@
             <div id="comments"><?php comments_template(); ?></div>
 			
 			<div id="pagenav">
-                <div class="alignleft">
-                	<?php next_post_link('%link','<div class="pagenav_disc"><div class="genericon pagenav_leftarrow"></div></div><div class="pagenav_link">%title</div>',0) ?>
+                <div class="alignleft pagenav_link_block">
+                	<?php next_post_link('%link','<div class="genericon pagenav_leftarrow"></div><div class="pagenav_link">%title</div>',0) ?>
                 </div>
-                <div class="alignright">
-                	<?php previous_post_link('%link','<div class="pagenav_link">%title</div><div class="pagenav_disc"><div class="genericon pagenav_rightarrow"></div></div>',0) ?>
+                <div class="alignright pagenav_link_block">
+                	<?php previous_post_link('%link','<div class="pagenav_link">%title</div><div class="genericon pagenav_rightarrow"></div>',0) ?>
                 </div>
+                <div class="clear"></div>
             </div>
 
 		<?php endwhile;
