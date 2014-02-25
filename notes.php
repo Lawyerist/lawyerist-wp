fe77a6575fa72b964d86b956806fe03f686f34b0
Sam Glover <sam@samglover.net>
2014-02-24 22:53:15 -0600

    Front page tweaks.

--- notes.php
+++ notes.php
@@ -1,6 +1,11 @@
 <?php 
 
-if ( has_tag('law-firm-websites') || has_tag('blogging') ) { ?>
-	<p class="note">Get a <em>great</em> law firm website or blog from Lawyerist Sites.<br />
-	<a href="http://sites.lawyerist.com/?utm_source=Lawyerist&utm_medium=text-link&utm_campaign=sites_note" rel="nofollow">Learn more.</a></p>
+if ( has_tag('law-firm-websites') ) { ?>
+	<p class="note">Get a <em>great</em> law firm website from Lawyerist Sites, starting at $500.<br />
+	<a href="http://sites.lawyerist.com/websites/?utm_source=Lawyerist&utm_medium=text-link&utm_campaign=sites_note" rel="nofollow">Learn more.</a></p>
+<?php }
+
+if ( has_tag('blogging') ) { ?>
+	<p class="note">Clean, simple, responsive law blogs from Lawyerist Sites, just $20/month.<br />
+	<a href="http://sites.lawyerist.com/law-blogs/?utm_source=Lawyerist&utm_medium=text-link&utm_campaign=sites_note" rel="nofollow">Learn more.</a></p>
 <?php }
