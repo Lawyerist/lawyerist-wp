71951b52150293aa6575248c6ea85e5761783cfb
Sam Glover <sam@samglover.net>
2014-04-08 12:26:08 -0500

    Remove need for "no image" tag on slideshows.

--- footer.php
+++ footer.php
@@ -2,18 +2,14 @@
 <div id="footer">
 	<div id="footer_content">
 		<p>Lawyerist is a daily online magazine about law practice. We help lawyers find the tools to improve their practices, we lead a conversation about law practice, and we have a strong online community of lawyers, the <a href="http://lab.lawyerist.com">Lawyerist Lab</a>.</p>
-
-		<p>Lawyerist is edited and written mainly by <a href="http://lawyerist.com/author/samglover/" rel="nofollow">Sam Glover</a>, with the help of a bunch of other people. Some are practicing lawyers, some are former practicing lawyers who are now doing other things related to law practice, and one or two aren't lawyers at all. Because lawyers don't know everything.
-
-		<p>Lawyerist.com is designed in-house and built on <a href="http://wordpress.org/" rel="nofollow">WordPress</a>. WordPress is awesome. Headlines are set in <a href="http://www.google.com/fonts/specimen/Roboto+Slab" rel="nofollow">Roboto Slab</a>. Body text is <a href="http://www.google.com/fonts/specimen/Source+Sans+Pro" rel="nofollow">Source Sans Pro</a>. Icons are <a href="http://genericons.com/" rel="nofollow">Genericons</a>.</p>
 	</div>
 	<div id="footer_legal">
 		<p class="text_right remove_bottom">The original content within this website is &copy; 2007&ndash;<?php echo date("Y") ?></p>
 		<p class="text_right"><a href="<?php echo home_url(); ?>/privacy-policy/">Privacy policy</a> / <a href="<?php echo home_url(); ?>/ftc-disclosures/">FTC disclosures</a> / <a href="<?php echo home_url(); ?>/sitemap_index.xml">XML sitemap</a></p>
     </div>
-	
+
 	<?php wp_footer(); ?>
-	
+
 </div>
 </div>
 
