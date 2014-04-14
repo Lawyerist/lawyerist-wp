c19be6f4432e50ed0c6806b8e16dbb8646a2c30e
Sam Glover <sam@samglover.net>
2014-04-14 09:04:50 -0500

    Quick fix for multiple notes added to posts by tag

--- notes.php
+++ notes.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 if ( has_tag('law-firm-websites') ) { ?>
 	<p class="note">Get a <em>great</em> law firm website from Lawyerist Sites, starting at $500.<br />
 	<a href="http://sites.lawyerist.com/websites/?utm_source=Lawyerist&utm_medium=text-link&utm_campaign=sites_note" rel="nofollow">Learn more.</a></p>
 <?php }
 
-if ( has_tag('blogging') ) { ?>
+if ( has_tag('blogging') && !has_tag('law-firm-websites') ) { ?>
 	<p class="note">Clean, simple, responsive law blogs from Lawyerist Sites, just $20/month.<br />
 	<a href="http://sites.lawyerist.com/law-blogs/?utm_source=Lawyerist&utm_medium=text-link&utm_campaign=sites_note" rel="nofollow">Learn more.</a></p>
 <?php }
