47c45637e634b37ef26b2a6b4fb73c5968570fcf
Sam Glover <sam@samglover.net>
2014-04-14 14:58:58 -0500

    Put popular posts widget back on front page

--- sidebar.php
+++ sidebar.php
@@ -52,29 +52,27 @@
 	<div class="clear"></div>
 </li>
 
-<?php if ( !is_page('featured') ) { ?>
 
-	<li id="popular_posts_tabbed">
-			<h3>Popular Posts</h3>        
-			<ul class="idTabs"> 
-					<li><a href="#current">This Week</a></li> 
-					<li><a href="#all-time">All-Time</a></li> 
-					<li><a href="#our-picks">Our Picks</a></li>
-			</ul>
+<li id="popular_posts_tabbed">
+		<h3>Popular Posts</h3>
+		<ul class="idTabs">
+				<li><a href="#current">This Week</a></li>
+				<li><a href="#all-time">All-Time</a></li>
+				<li><a href="#our-picks">Our Picks</a></li>
+		</ul>
 
-			<div id="current" class="tabs_sublist">
-					<?php wpp_get_mostpopular("post_type='post'&range=weekly&limit=5&stats_comments=0&thumbnail_height=75&thumbnail_width=75&post_html='<li>{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a></li>'"); ?>
-			</div> 
+		<div id="current" class="tabs_sublist">
+				<?php wpp_get_mostpopular("post_type='post'&range=weekly&limit=5&stats_comments=0&thumbnail_height=75&thumbnail_width=75&post_html='<li>{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a></li>'"); ?>
+		</div>
 
-			<div id="all-time" class="tabs_sublist">
-					<?php wpp_get_mostpopular("post_type='post'&range=all&limit=5&stats_comments=0&thumbnail_height=75&thumbnail_width=75&post_html='<li>{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a></li>'"); ?>
-			</div>
+		<div id="all-time" class="tabs_sublist">
+				<?php wpp_get_mostpopular("post_type='post'&range=all&limit=5&stats_comments=0&thumbnail_height=75&thumbnail_width=75&post_html='<li>{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a></li>'"); ?>
+		</div>
 
-			<div id="our-picks" class="tabs_sublist">
-					<?php wpp_get_mostpopular("post_type='post'&cat='2621'&range=all&limit=5&stats_comments=0&thumbnail_height=75&thumbnail_width=75&post_html='<li>{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a></li>'"); ?>
-			</div>
-	</li>
+		<div id="our-picks" class="tabs_sublist">
+				<?php wpp_get_mostpopular("post_type='post'&cat='2621'&range=all&limit=5&stats_comments=0&thumbnail_height=75&thumbnail_width=75&post_html='<li>{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a></li>'"); ?>
+		</div>
+</li>
 
-<?php } ?>
 
 <?php dynamic_sidebar('sidebar_1'); ?>
