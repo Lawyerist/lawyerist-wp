6f7699aada310d8ad34b4ac20cb7a784ca44d14f
Sam Glover <sam@samglover.net>
2014-05-09 11:56:36 -0500

    Align tags list on posts & adjust PrintFriendly button positioning

--- head.php
+++ head.php
@@ -2,7 +2,7 @@
 
 <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic|Roboto+Slab:700,400' rel='stylesheet' type='text/css'>
 
-<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>?2014-05-08-15-37" type="text/css" media="screen, projection">
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>?2014-05-09-11-56" type="text/css" media="screen, projection">
 <link rel="shortcut icon" href="<?php echo get_bloginfo('template_url'); ?>/images/favicon.ico" type="image/x-icon">
 <link rel="icon" href="<?php echo get_bloginfo('template_url'); ?>/images/favicon.ico" type="image/x-icon">
 
