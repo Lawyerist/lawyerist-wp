36fcc76860f3ce998ce2cb18d9fe22787aa7a340
Sam Glover <sam@samglover.net>
2014-05-10 07:58:01 -0500

    Remove Sites Network box from front page

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # A custom WordPress theme for Lawyerist.com
 
+2014-05-10
+----------
+
+* Remove Sites Network box from front page.
+* Increase number of featured posts shown.
+
 2014-05-09
 ----------
 
--- page-featured.php
+++ page-featured.php
@@ -47,7 +47,7 @@
 
   		<?php /* THE LOOP */
 
-  			$featured_query = new WP_Query( 'posts_per_page=7' );
+  			$featured_query = new WP_Query( 'posts_per_page=11' );
 
   			$post_num = 1;
 
@@ -145,42 +145,6 @@
         </ul>
       </div><!--end #lab_posts-->
 		</div>
-
-		<div class="fp_tab"><h2>Lawyerist Sites Network</h2></div>
-		<div id="sites_network_posts">
-			<?php // Get RSS Feed(s)
-			include_once( ABSPATH . WPINC . '/feed.php' );
-
-			// Get a SimplePie feed object from the specified feed source.
-			$rss = fetch_feed( 'http://sites.lawyerist.com/feed/globalpostsfeed' );
-
-			if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
-
-				// Figure out how many total items there are, but limit it to 5.
-				$maxitems = $rss->get_item_quantity( 5 );
-
-				// Build an array of all the items, starting with element 0 (first element).
-				$rss_items = $rss->get_items( 0, $maxitems );
-
-			endif;
-			?>
-
-      <ul>
-      	<?php if ( $maxitems == 0 ) : ?>
-      		<li><?php _e( 'No items', 'my-text-domain' ); ?></li>
-      	<?php else : ?>
-      		<?php // Loop through each feed item and display each item as a hyperlink. ?>
-      		<?php foreach ( $rss_items as $item ) : ?>
-      			<li>
-      				<a href="<?php echo esc_url( $item->get_permalink() ); ?>"
-      					title="<?php printf( __( 'Posted on %s', 'my-text-domain' ), $item->get_date('F jS, Y') ); ?>">
-      					<?php echo esc_html( $item->get_title() ); ?>
-      				</a>
-      			</li>
-      		<?php endforeach; ?>
-      	<?php endif; ?>
-      </ul>
-    </div><!--end #sites_network_posts-->
 
 	</div><!--end content_column-->
 
