58b15c3b955983697e370b7760b89b00b9becea4
Sam Glover <sam@samglover.net>
2014-07-31 12:44:26 -0500

    Add categories section to front page

--- page-featured.php
+++ page-featured.php
@@ -42,7 +42,7 @@
 
     /* END PINNED POST LOOP */ ?>
 
-  <div class="fp_tab"><h2>Latest Posts</h2></div>
+    <div class="fp_tab"><h2>Latest Posts</h2></div>
   	<div id="featured_posts">
 
   		<?php /* FEATURED POSTS LOOP */
@@ -89,20 +89,77 @@
 
   			endwhile;
 
-			/* END FEATURED POSTS LOOP */ ?>
+  		/* END FEATURED POSTS LOOP */ ?>
 
-			<div class="clear"></div>
+  		<div class="clear"></div>
 
-		</div><!--end #featured_posts-->
+  	</div><!--end #featured_posts-->
 
-		<div id="read_latest_posts">
-			<a href="<?php echo bloginfo('url') . '/articles/'; ?>">
-				<p>Read all posts &rarr;</p>
-			</a>
-		</div>
+  	<div id="read_latest_posts">
+  		<a href="<?php echo bloginfo('url') . '/articles/'; ?>">
+  			<p>Read all posts &rarr;</p>
+  		</a>
+  	</div>
 
-		<div class="fp_tab"><h2>Discussions</h2></div>
-		<div id="discussions_container">
+
+    <div class="fp_tab"><h2>Categories</h2></div>
+    <div id="popular_in_cats">
+
+      <div class="cat_post left">
+        <h3><a href="http://lawyerist.com/topic/practice-management/">Practice Management</a></h3>
+        <?php wpp_get_mostpopular("wpp_start=' '&post_type='post'&cat='362'&range='all'&limit=1&stats_comments=0&thumbnail_height=150&thumbnail_width=269&post_html='{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a>'&wpp_end=' '"); ?>
+      </div>
+
+      <div class="cat_post">
+        <h3><a href="http://lawyerist.com/topic/starting-a-law-firm/">Starting a Law Firm</a></h3>
+        <?php wpp_get_mostpopular("wpp_start=' '&post_type='post'&cat='708'&range='all'&limit=1&stats_comments=0&thumbnail_height=150&thumbnail_width=269&post_html='{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a>'&wpp_end=' '"); ?>
+      </div>
+
+      <div class="clear"></div>
+
+
+      <div class="cat_post left">
+        <h3><a href="http://lawyerist.com/topic/tech/">Tech</a></h3>
+        <?php wpp_get_mostpopular("wpp_start=' '&post_type='post'&cat='10'&range='all'&limit=1&stats_comments=0&thumbnail_height=150&thumbnail_width=269&post_html='{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a>'&wpp_end=' '"); ?>
+      </div>
+
+      <div class="cat_post">
+        <h3><a href="http://lawyerist.com/topic/marketing/">Marketing</a></h3>
+        <?php wpp_get_mostpopular("wpp_start=' '&post_type='post'&cat='6'&range='all'&limit=1&stats_comments=0&thumbnail_height=150&thumbnail_width=269&post_html='{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a>'&wpp_end=' '"); ?>
+      </div>
+
+      <div class="clear"></div>
+
+
+      <div class="cat_post left">
+        <h3><a href="http://lawyerist.com/topic/lawyering-skills/">Lawyering Skills</a></h3>
+        <?php wpp_get_mostpopular("wpp_start=' '&post_type='post'&cat='886'&range='all'&limit=1&stats_comments=0&thumbnail_height=150&thumbnail_width=269&post_html='{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a>'&wpp_end=' '"); ?>
+      </div>
+
+      <div class="cat_post">
+        <h3><a href="http://lawyerist.com/topic/ethics/">Ethics</a></h3>
+        <?php wpp_get_mostpopular("wpp_start=' '&post_type='post'&cat='132'&range='all'&limit=1&stats_comments=0&thumbnail_height=150&thumbnail_width=269&post_html='{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a>'&wpp_end=' '"); ?>
+      </div>
+
+      <div class="clear"></div>
+
+      <div class="cat_post left">
+        <h3><a href="http://lawyerist.com/topic/lifestyle/">Lifestyle</a></h3>
+        <?php wpp_get_mostpopular("wpp_start=' '&post_type='post'&cat='2622'&range='all'&limit=1&stats_comments=0&thumbnail_height=150&thumbnail_width=269&post_html='{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a>'&wpp_end=' '"); ?>
+      </div>
+
+      <div class="cat_post">
+        <h3><a href="http://lawyerist.com/topic/law-school+careers/">Law School & Careers</a></h3>
+        <?php wpp_get_mostpopular("wpp_start=' '&post_type='post'&cat='707,743'&range='all'&limit=1&stats_comments=0&thumbnail_height=150&thumbnail_width=269&post_html='{thumb}<a class=\"wpp_headline\" href=\"{url}?utm_source=lawyerist_sidebar\">{text_title}</a>'&wpp_end=' '"); ?>
+      </div>
+
+      <div class="clear"></div>
+
+    </div><!--end #popular_in_cats-->
+
+
+  	<div class="fp_tab"><h2>Discussions</h2></div>
+  	<div id="discussions_container">
       <div id="most_discussed">
         <h3>Most-Discussed Posts</h3>
   			<?php wpp_get_mostpopular("post_type='post'&range=monthly&order_by=comments&limit=3&thumbnail_height=60&thumbnail_width=60&post_html='<li><div class=\"wpp_thumb\">{thumb}</div><a class=\"wpp_headline\" href=\"{url}\">{text_title}</a><a class=\"comment_link\" href=\"{url}#comments\" rel=\"nofollow\">{comments} recent comments</a><div class=\"clear\"></div></li>'"); ?>
@@ -146,56 +203,56 @@
         </ul>
       </div><!--end #lab_posts-->
       <div class="clear"></div>
-		</div>
+  	</div>
 
     <div class="fp_tab"><h2>Bookstore</h2></div>
-      <div id="bookstore">
+    <div id="bookstore">
 
-        <?php /* BOOKSTORE POSTS LOOP */
+      <?php /* BOOKSTORE POSTS LOOP */
 
-          $args = array(
-            'posts_per_page' => 2,
-            'tag' => 'ebooks'
-          );
+        $args = array(
+          'posts_per_page' => 2,
+          'tag' => 'ebooks'
+        );
 
-          $bookstore_query = new WP_Query( $args );
+        $bookstore_query = new WP_Query( $args );
 
-          $post_num = 2;
+        $post_num = 2;
 
-          while ( $bookstore_query->have_posts() ) : $bookstore_query->the_post();
+        while ( $bookstore_query->have_posts() ) : $bookstore_query->the_post();
 
-            if ( $post->ID == $do_not_duplicate ) continue;
+          if ( $post->ID == $do_not_duplicate ) continue;
 
-            $do_not_duplicate = $post->ID;
+          $do_not_duplicate = $post->ID;
 
-            $num_comments = get_comments_number();
-            $classes = array(
-              'bookstore_post',
-              'post_num_' . $post_num
-            ); ?>
+          $num_comments = get_comments_number();
+          $classes = array(
+            'bookstore_post',
+            'post_num_' . $post_num
+          ); ?>
 
-            <a id="post-<?php the_ID(); ?>" <?php post_class($classes); ?> href="<?php the_permalink(); ?>?utm_source=lawyerist_front_page" rel="bookmark" title="<?php the_title(); ?>, posted on <?php the_time('F jS, Y'); ?>">
+          <a id="post-<?php the_ID(); ?>" <?php post_class($classes); ?> href="<?php the_permalink(); ?>?utm_source=lawyerist_front_page" rel="bookmark" title="<?php the_title(); ?>, posted on <?php the_time('F jS, Y'); ?>">
 
-              <div class="headline_excerpt">
-                <h2 class="headline"><?php the_title(); ?></h2>
-              </div><!--end .headline_excerpt-->
+            <div class="headline_excerpt">
+              <h2 class="headline"><?php the_title(); ?></h2>
+            </div><!--end .headline_excerpt-->
 
-              <div class="shadowbox"></div>
-              <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'medium' ); } ?>
-              <div class="clear"></div>
+            <div class="shadowbox"></div>
+            <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'medium' ); } ?>
+            <div class="clear"></div>
 
-            </a>
+          </a>
 
-            <?php if ( $post_num==3 ) { $post_num = 2; }
-            else { $post_num++; }
+          <?php if ( $post_num==3 ) { $post_num = 2; }
+          else { $post_num++; }
 
-          endwhile;
+        endwhile;
 
-        /* END BOOKSTORE POSTS LOOP */ ?>
+      /* END BOOKSTORE POSTS LOOP */ ?>
 
-        <div class="clear"></div>
+      <div class="clear"></div>
 
-      </div><!--end #bookstore_posts-->
+    </div><!--end #bookstore_posts-->
 
 	</div><!--end content_column-->
 
