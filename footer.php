daf41c0721344124fb45138c0b0157b9797227b3
Sam Glover <sam@samglover.net>
2014-10-27 18:23:31 -0500

    Add About page to footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 	<div id="footer_legal">
 		<p class="text_right">The original content within this website is &copy; 2007&ndash;<?php echo date("Y") ?>.</p>
 		<p class="text_right">LAWYERIST, LAWYERIST LAB, and LAWYERIST SITES<br />are trademarks registered by Lawyerist Media, LLC.</p>
-		<p class="text_right"><a href="<?php echo home_url(); ?>/privacy-policy/">Privacy policy</a> / <a href="<?php echo home_url(); ?>/ftc-disclosures/">FTC disclosures</a> / <a href="<?php echo home_url(); ?>/sitemap_index.xml">XML sitemap</a></p>
+		<p class="text_right"><a href="<?php echo home_url(); ?>/about/">About</a> / <a href="<?php echo home_url(); ?>/privacy-policy/">Privacy policy</a> / <a href="<?php echo home_url(); ?>/ftc-disclosures/">FTC disclosures</a> / <a href="<?php echo home_url(); ?>/sitemap_index.xml">XML sitemap</a></p>
   </div>
 
 	<?php wp_footer(); ?>
