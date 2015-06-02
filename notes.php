1caf0a5c1ced9e5e71cf55e5f69a670278c71136
Sam Glover <sam@samglover.net>
2015-06-01 22:17:56 -0500

    Add note for security guide

--- notes.php
+++ notes.php
@@ -1,5 +1,7 @@
 <?php
 
+/* Website Design Guide */
+
 $websites_tags = array(
 	'blogging',
 	'best-law-blog-posts',
@@ -16,6 +18,24 @@ if ( has_tag( $websites_tags ) || is_page( $websites_pages ) ) { ?>
 		<h3>Get our White Paper, "10 Things the Best Law-Firm Website Designs Have in Common"</h3>
 		<p>For the past five years, Lawyerist has published an annual list of the best law firm websites. Now, you can find out what they have in common.</p>
 		<a href="https://lawyerist.com/law-firm-website-design-white-paper/" class="button">Get it Now!</a>
+		<div class="clear"></div>
+	</div>
+<?php }
+
+
+/* Computer Security Guide */
+
+$security_tags = array(
+	'security',
+	'data-security'
+);
+
+if ( has_tag( $security_tags ) ) { ?>
+	<div class="white_paper_note">
+		<a href="https://lawyerist.com/downloads/4-step-computer-security-upgrade/"><img class="alignright size-medium wp-image-82487" style="border: 1px solid #ddd;" src="https://lawyerist.com/lawyerist/wp-content/uploads/2015/05/computer-security-upgrade-cover-shop-300x388.png" alt="computer-security-upgrade-cover-shop" width="300" height="388" /></a>
+		<h3>Get our "4-Step Computer Security Upgrade"</h3>
+		<p>This guide makes it as easy to lock the virtual door to your computer as it is to lock the physical door to your office. In less than an hour, you will learn to encrypt your files, secure your computer when using public Wi-Fi, enable two-factor authentication, and use good passwords.</p>
+		<a href="https://lawyerist.com/downloads/4-step-computer-security-upgrade/" class="button">Get it Now!</a>
 		<div class="clear"></div>
 	</div>
 <?php }
