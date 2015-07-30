7f1d082dbe70cf95b52d99a2ea4fd6e9017cd019
Sam Glover <sam@samglover.net>
2015-07-30 06:13:50 -0500

    Update website design guide note

--- notes.php
+++ notes.php
@@ -45,8 +45,8 @@ if ( has_tag( $security_tags ) ) { ?>
 elseif ( has_tag( $web_design_tags ) || is_page( $web_design_pages ) ) { ?>
 
 	<div class="survival_guide_note">
-		<a href="https://lawyerist.com/downloads/law-firm-website-design-guide/"><img src="https://lawyerist.com/lawyerist/wp-content/uploads/2015/05/website_design_guide_cover-300x306.png" alt="website_design_guide_cover" width="300" height="306" class="alignright size-medium wp-image-84254" /></a>
-		<h3>Get our White Paper, "10 Things the Best Law-Firm Website Designs Have in Common"</h3>
+		<a href="https://lawyerist.com/downloads/law-firm-website-design-guide/"><img src="https://lawyerist.com/lawyerist/wp-content/uploads/edd/2015/07/website-design-guide-cover-300x306.png" alt="website-design-guide-cover" width="300" height="306" class="alignright size-medium wp-image-86169" /></a>
+		<h3>Get our guide, "10 Things the Best Law-Firm Website Designs Have in Common"</h3>
 		<p>For the past five years, Lawyerist has published an annual list of the best law firm websites. Now, you can find out what they have in common.</p>
 		<a href="https://lawyerist.com/downloads/law-firm-website-design-guide/" class="button">Get it Now!</a>
 		<div class="clear"></div>
