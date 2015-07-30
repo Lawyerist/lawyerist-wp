00bac27a9adfaa82dd893e35357c09d3cd75e2d4
Sam Glover <sam@samglover.net>
2015-07-30 09:45:23 -0500

    Update notes; make linked images active

--- notes.php
+++ notes.php
@@ -30,7 +30,7 @@ $web_design_pages = array(
 if ( has_tag( $security_tags ) ) { ?>
 
 	<div class="survival_guide_note">
-		<a href="https://lawyerist.com/downloads/4-step-computer-security-upgrade/"><img class="alignright size-medium wp-image-82487" src="https://lawyerist.com/lawyerist/wp-content/uploads/edd/2015/06/computer-security-upgrade-cover-shop-300x306.png" alt="computer-security-upgrade-cover-shop" width="300" height="306" /></a>
+		<a href="https://lawyerist.com/downloads/4-step-computer-security-upgrade/"><img src="https://lawyerist.com/lawyerist/wp-content/uploads/2015/05/computer-security-guide-cover-2-300x391.png" alt="computer-security-guide-cover-2" width="300" height="391" class="alignright size-medium wp-image-86178" /></a>
 		<h3>Get our "4-Step Computer Security Upgrade"</h3>
 		<p>This guide makes it as easy to lock the virtual door to your computer as it is to lock the physical door to your office. In less than an hour, you will learn to encrypt your files, secure your computer when using public Wi-Fi, enable two-factor authentication, and use good passwords.</p>
 		<a href="https://lawyerist.com/downloads/4-step-computer-security-upgrade/" class="button">Get it Now!</a>
@@ -45,7 +45,7 @@ if ( has_tag( $security_tags ) ) { ?>
 elseif ( has_tag( $web_design_tags ) || is_page( $web_design_pages ) ) { ?>
 
 	<div class="survival_guide_note">
-		<a href="https://lawyerist.com/downloads/law-firm-website-design-guide/"><img src="https://lawyerist.com/lawyerist/wp-content/uploads/edd/2015/07/website-design-guide-cover-300x306.png" alt="website-design-guide-cover" width="300" height="306" class="alignright size-medium wp-image-86169" /></a>
+		<a href="https://lawyerist.com/downloads/law-firm-website-design-guide/"><img src="https://lawyerist.com/lawyerist/wp-content/uploads/2015/05/website-design-guide-cover-2-300x391.png" alt="website-design-guide-cover-2" width="300" height="391" class="alignright size-medium wp-image-86177" /></a>
 		<h3>Get our guide, "10 Things the Best Law-Firm Website Designs Have in Common"</h3>
 		<p>For the past five years, Lawyerist has published an annual list of the best law firm websites. Now, you can find out what they have in common.</p>
 		<a href="https://lawyerist.com/downloads/law-firm-website-design-guide/" class="button">Get it Now!</a>
