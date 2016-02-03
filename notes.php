58c95f23208d478e4dea44652004b22244e2b330
Sam Glover <sam@samglover.net>
2016-02-02 18:43:25 -0600

    Add note for computer setup guide.

--- notes.php
+++ notes.php
@@ -15,6 +15,12 @@ $security_tags = array(
 );
 
 
+$computer_setup_tags = array(
+	'microsoft',
+	'windows'
+);
+
+
 $web_design_tags = array(
 	'blogging',
 	'best-law-blog-posts',
@@ -34,6 +40,21 @@ if ( has_tag( $security_tags ) ) { ?>
 		<h3>Get our "4-Step Computer Security Upgrade"</h3>
 		<p>This guide makes it as easy to lock the virtual door to your computer as it is to lock the physical door to your office. In less than an hour, you will learn to encrypt your files, secure your computer when using public Wi-Fi, enable two-factor authentication, and use good passwords.</p>
 		<a href="https://lawyerist.com/downloads/4-step-computer-security-upgrade/" class="button">Get it Now!</a>
+		<div class="clear"></div>
+	</div>
+
+<?php }
+
+
+/* Computer Setup Guide */
+
+elseif ( has_tag( $computer_setup_tags ) ) { ?>
+
+	<div class="computer_setup_guide_note" data-swiftype-index="false">
+		<a href="https://lawyerist.com/downloads/windows-computer-setup-guide/"><img src="https://lawyerist.com/lawyerist/wp-content/uploads/edd/2016/02/computer-setup-guide-cover-300x391.png" alt="computer-setup-guide-cover-2" width="300" height="391" class="alignright size-medium wp-image-86177" /></a>
+		<h3>Get our guide, "How to Set Up Your New Windows Computer"</h3>
+		<p>A brand-new Windows PC, fully updated and unsullied by crapware, is a wonderful thing. Sadly, very few people ever get to experience it—but you can!</p>
+		<a href="https://lawyerist.com/downloads/windows-computer-setup-guide/" class="button">Get it Now!</a>
 		<div class="clear"></div>
 	</div>
 
