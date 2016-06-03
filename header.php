1b2d74091ec20a354df64ba68740e0ab9df5e1d9
Sam Glover <sam@samglover.net>
2016-06-03 14:28:46 -0500

    Make header signup form properly responsive.

--- header.php
+++ header.php
@@ -37,7 +37,9 @@
 		<div id="header_signup">
 			<div id="mc_embed_signup">
 				<form action="//lawyerist.us2.list-manage.com/subscribe/post?u=a5da2382c098b6541dcd6cf8e&amp;id=30d7a1f6e2&SOURCE=header_signup_02_orange" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-					<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
+					<div class="email_input_container">
+						<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email address" required>
+					</div>
 					<!-- Real people should not fill this in and expect good things—do not remove this or risk form bot signups. -->
 					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_a5da2382c098b6541dcd6cf8e_30d7a1f6e2" tabindex="-1" value=""></div>
 					<input type="submit" value="Subscribe!" name="subscribe" id="mc-embedded-subscribe" class="button">
