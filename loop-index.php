944ee197bac1208d7fcfd5552a1ec23c1310c8bb
Sam Glover <sam@samglover.net>
2016-11-07 16:48:26 -0600

    Saving WIP.

--- loop-index.php
+++ loop-index.php
@@ -1,15 +1,43 @@
 <?php
 
-$post_num     = 1; // Counter for inserting mobile ads.
 
+/******************************
+Selectors
+
+has_tag( 'lawyerist-podcast' )
+has_term( true, 'series' )
+has_term( true, 'sponsor' )
+get_post_type( $post->ID ) == 'download'
+get_post_type( $post->ID ) == 'page'
+get_post_type( $post->ID ) == 'aside'
+
+******************************/
+
+// Start the Loop.
 if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-  // Group a post that is in a series with other posts in that series.
+  // Assign post variables.
+  $post_num = 1; // Counter for inserting mobile ads.
+  $post_title   = the_title( '', '', FALSE );
+  $post_url     = get_permalink();
+  $post_classes = []; // .post, .page, and .download are added automatically, as are tags and formats.
+
+  // Assign classes.
   if ( has_term( true, 'series' ) ) {
+    $post_classes[] = 'series';
+  }
 
-    echo '<div class="series_post_container">';
+  if ( has_term( true, 'sponsor' ) ) {
+    $post_classes[] = 'sponsored';
+  }
 
-      $this_post[] = $post->ID;
+
+  // This is the container for all posts/groups of posts.
+  echo '<div class="index_post_container">';
+
+    // Series title:
+    // Check for a series, get the series information, then output the series title.
+    if ( has_term( true, 'series' ) ) {
 
       $series_ID = wp_get_post_terms(
         $post->ID,
@@ -27,31 +55,65 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
       $series_slug				= $series_info->slug;
       $series_url					=	get_term_link( $series_ID[0], 'series' );
 
-      $post_title   = the_title( '', '', FALSE );
-      $post_excerpt = get_the_excerpt();
-      $post_url     = get_permalink();
-
       echo '<h2 class="series_title"><a href="' . $series_url . '" title="' . $series_title . '">' . $series_title . '</a></h2>';
 
-      echo '<a ';
-      post_class( 'post_in_series' );
-      echo 'href="' . $post_url . '" title="' . $post_title . '">';
+    }
 
-        if ( has_post_thumbnail() ) { the_post_thumbnail( 'aside_thumbnail' ); }
 
-        echo '<div class="headline_excerpt">';
+    // Post link container (.post). The whole thing is a link!
+    echo '<a href="' . $post_url . '" title="' . $post_title . '"';
+    post_class( $post_classes );
+    echo '>';
 
-          echo '<h2 class="headline">' . $post_title . '</h2>';
+      // Show the post image if there is one.
+      if ( has_post_thumbnail() ) {
 
-          if ( !is_mobile() ) { echo '<p class="excerpt">' . $post_excerpt . '</p>'; }
+        if ( has_post_format( 'aside' ) ) {
+          the_post_thumbnail( 'aside_thumbnail' );
+        } elseif ( has_post_thumbnail() ) {
+          the_post_thumbnail( 'download_thumbnail' );
+        } elseif ( get_post_type( $post->ID ) == 'page' ) {
+          the_post_thumbnail( 'thumbnail' );
+        } else {
+          the_post_thumbnail( 'standard_thumbnail' );
+        }
 
+      }
+
+      echo '<div class="headline_excerpt">';
+
+        // Headline
+        echo '<h2 class="headline">' . $post_title . '</h2>';
+
+        // Output the excerpt unless we're showing a page.
+        if ( get_post_type( $post->ID ) != 'page' ) {
+
+          $post_excerpt = get_the_excerpt();
+
+          echo '<p class="excerpt">' . $post_excerpt . '</p>';
+
+        }
+
+        // Output the post meta unless we're showing a page or a download.
+        if ( get_post_type( $post->ID ) != ( 'page' || 'download' ) ) {
           lawyerist_get_postmeta();
+        }
 
-        echo '</div>'; // End .headline_excerpt.
+      echo '</div>'; // Close .headline_excerpt.
 
-      echo '</a>';
+    echo '</a>'; // This closes the post link container (.post).
 
-      // Begin series loop.
+
+    // Show a button for downloads.
+    if ( get_post_type( $post->ID ) == 'download' ) {
+      echo edd_get_purchase_link( array( 'download_id' => $post->ID ) );
+    }
+
+
+    // If the post is in a series, show up to 4 additional posts in that series.
+    if ( has_term( true, 'series' ) ) {
+
+      $this_post[] = $post->ID; // We use this to exclude the current post.
 
       $series_query_args = array(
         'orderby'					=> 'date',
@@ -69,7 +131,9 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 
       $series_query = new WP_Query( $series_query_args );
 
-      if ( $series_query->post_count > 1 ) {
+      // Start the series sub-Loop.
+
+      if ( $series_query->post_count > 1 ) :
 
         echo '<ul>';
 
@@ -78,7 +142,7 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
             $post_title = the_title( '', '', FALSE );
             $post_url   = get_permalink();
 
-            echo '<li><a class="post_in_series" href="' . $post_url . '" title="' . $post_title . '"><h3 class="headline">' . $post_title . '</h3></a></li>';
+            echo '<li><a class="series" href="' . $post_url . '" title="' . $post_title . '"><h3 class="headline">' . $post_title . '</h3></a></li>';
 
           endwhile;
 
@@ -86,95 +150,21 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 
         echo '<div class="clear"></div>';
 
-      } // End series loop.
+      endif; // End series sub-Loop.
 
       wp_reset_postdata(); // Necessary because the series loop is nested in the main loop.
 
-    echo '</div>'; // End #series_post_container.
+    } // End the additional series posts.
 
-  // End series post.
 
-  // Downloads (need different formatting so the button shows up and is clickable.)
-  } elseif ( get_post_type( $post->ID ) == 'download' ) {
+  echo '</div>'; // Close .index_post_container.
 
-    $post_title   = the_title( '', '', FALSE );
-    $post_excerpt = get_the_excerpt();
-    $post_url     = get_permalink();
+  // Insert ads on mobile.
+  if ( $post_num == 1 && is_mobile() ) { lawyerist_get_ap2(); }
+  if ( $post_num == 3 && is_mobile() ) { lawyerist_get_ap3(); }
 
-    echo '<div ';
-    echo post_class( 'download_post_container' );
-    echo '>';
+  $post_num++; // Increment counter.
 
-      echo '<a href="' . $post_url . '" title="' . $post_title . '">';
-
-        if ( has_post_thumbnail() ) { the_post_thumbnail( 'download_thumbnail' ); }
-
-        echo '<div class="headline_excerpt">';
-
-          echo '<h2 class="headline">' . $post_title . '</h2>';
-
-          echo '<p class="excerpt">' . $post_excerpt . '</p>';
-
-        echo '</div>'; // End .headline_excerpt.
-
-      echo '</a>';
-
-      echo edd_get_purchase_link( array( 'download_id' => $post->ID ) );
-
-    echo '</div>'; // End #download_post_container.
-
-  // End download.
-
-  // Remaining post types/formats.
-  } else {
-
-    $post_title   = the_title( '', '', FALSE );
-    $post_excerpt = get_the_excerpt();
-    $post_url     = get_permalink();
-    $post_classes = [];
-
-    if ( has_term( true, 'sponsor' ) ) { $post_classes[] = 'sponsored_post'; }
-
-    echo '<a ';
-    post_class( $post_classes );
-    echo 'href="' . $post_url . '" title="' . $post_title . '">';
-
-      // Select the appropriate thumbnail based on post type/format.
-      if ( has_post_thumbnail() ) {
-
-        if ( has_post_format( 'aside' ) ) {
-          the_post_thumbnail( 'aside_thumbnail' );
-        } elseif ( get_post_type( $post->ID ) == 'page' ) {
-          the_post_thumbnail( 'thumbnail' );
-        } else {
-          the_post_thumbnail( 'standard_thumbnail' );
-        }
-
-      }
-
-      echo '<div class="headline_excerpt">';
-
-        echo '<h2 class="headline">' . $post_title . '</h2>';
-
-        if ( get_post_type( $post->ID ) != 'page' ) {
-          if ( !is_mobile() ) { echo '<p class="excerpt">' . $post_excerpt . '</p>'; }
-          lawyerist_get_postmeta();
-        }
-
-      echo '</div>'; // End .headline_excerpt.
-
-      echo '<div class="clear"></div>';
-
-    echo '</a>'; // End .post.
-
-  }
-
-// Insert ads on mobile.
-if ( $post_num == 1 && is_mobile() ) { lawyerist_get_ap2(); }
-if ( $post_num == 3 && is_mobile() ) { lawyerist_get_ap3(); }
-
-$post_num++; // Increment counter.
-
-endwhile; endif;
+endwhile; endif; // Close the Loop.
 
 ?>
