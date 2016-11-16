182e170e0527ed122ab13bf7f4171916297aca6d
Sam Glover <sam@samglover.net>
2016-11-15 21:35:47 -0600

    Re-add author to product spotlight bylines.

--- postmeta-index.php
+++ postmeta-index.php
@@ -8,7 +8,7 @@ echo '<div class="postmeta">';
   $author = get_the_author_meta( 'display_name' );
   $date   = get_the_time( 'F jS, Y' );
 
-  if ( has_term( true, 'sponsor' ) ) {
+  if ( has_term( true, 'sponsor' ) || has_category( 'sponsored-posts' ) ) {
 
     $sponsor_IDs = wp_get_post_terms(
       $post->ID,
@@ -23,7 +23,12 @@ echo '<div class="postmeta">';
     $sponsor_info = get_term( $sponsor_IDs[0] );
     $sponsor      = $sponsor_info->name;
 
-    echo '<span class="author sponsor">Sponsored by ' . $sponsor . '</span> ';
+    if ( has_category( 'sponsored-posts' ) ) {
+      echo '<span class="author sponsor">Sponsored by ' . $sponsor . '</span> ';
+    } else {
+      echo '<span class="author">By ' . $author . ',&nbsp;</span><span class="author sponsor">sponsored by ' . $sponsor . ',&nbsp;</span>';
+    }
+
     echo '<span class="date">on ' . $date . '</span>';
 
   } elseif ( $author == 'Lawyerist' ) {
