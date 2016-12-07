fa954307e4ce3f20b633a894a9b0344498c14d0c
Sam Glover <sam@samglover.net>
2016-12-06 23:33:39 -0600

    Saving WIP.

--- header.php
+++ header.php
@@ -46,9 +46,17 @@
 		</li>
 		<li id="main-menu-subscribe">
 			<a href="#">Subscribe</a>
+			<?php gravity_form( 14, false, false, false, '', true, 1010, true ); ?>
 		</li>
 		<li id="main-menu-search">
 			<a href="#">Search</a>
+			<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+			<label for="main-menu-search-box">
+				<span hidden class="screen-reader-text"><?php echo _x( 'Search Lawyerist.com: ', 'label' ) ?></span>
+			</label>
+			<input id="main-menu-search-box" type="search" class="search-field" name="s" placeholder="<?php echo esc_attr_x( 'Search Lawyerist.com …', 'placeholder' ) ?>" title="<?php echo esc_attr_x( 'Search Lawyerist.com', 'label' ) ?>" />
+			<input hidden type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+			</form>
 		</li>
 	</ul>
 </div>
