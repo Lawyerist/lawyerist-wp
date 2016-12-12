85f20ef3c07bf28af83728ae4b8011f124bc6e46
Sam Glover <sam@samglover.net>
2016-12-12 13:04:53 -0600

    Remove trackers from footer.php.

--- footer.php
+++ footer.php
@@ -33,28 +33,3 @@ Load scripts
 <!--
 Load trackers
 --------------------------->
-
-<!-- MailChimp Goal tracking -->
-<script type="text/javascript">
-	var $mcGoal = {'settings':{'uuid':'a5da2382c098b6541dcd6cf8e','dc':'us2'}};
-	(function() {
-		 var sp = document.createElement('script'); sp.type = 'text/javascript'; sp.async = true; sp.defer = true;
-		sp.src = ('https:' == document.location.protocol ? 'https://s3.amazonaws.com/downloads.mailchimp.com' : 'http://downloads.mailchimp.com') + '/js/goal.min.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sp, s);
-	})();
-</script>
-
-<!-- Drip -->
-<script type="text/javascript">
-  var _dcq = _dcq || [];
-  var _dcs = _dcs || {};
-  _dcs.account = '5816544';
-
-  (function() {
-    var dc = document.createElement('script');
-    dc.type = 'text/javascript'; dc.async = true;
-    dc.src = '//tag.getdrip.com/5816544.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(dc, s);
-  })();
-</script>
