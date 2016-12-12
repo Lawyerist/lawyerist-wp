831191885dbffd9986c66833e5942fbe5b4145fe
Sam Glover <sam@samglover.net>
2016-12-12 15:02:36 -0600

    Style nav menu email signup form.

--- header.php
+++ header.php
@@ -40,7 +40,17 @@
 		<li id="main-menu-subscribe" class="main-menu-item">
 			<a href="#">Subscribe</a>
 			<div class="main-menu-dropdown">
-				<?php gravity_form( 14, false, false, false, '', true, 1010, true ); ?>
+				<div id="mc_embed_signup">
+				<form action="//lawyerist.us2.list-manage.com/subscribe/post?u=a5da2382c098b6541dcd6cf8e&amp;id=30d7a1f6e2&SOURCE=nav_menu_signup" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<label for="mce-EMAIL">
+						<span hidden class="screen-reader-text"><?php echo _x( 'Enter email address:', 'label' ) ?></span>
+					</label>
+					<input type="email" value="" name="EMAIL" class="email subscribe-email-field" id="mce-EMAIL" placeholder="Email address" required>
+					<!-- Real people should not fill this in and expect good things—do not remove this or risk form bot signups. -->
+					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_a5da2382c098b6541dcd6cf8e_30d7a1f6e2" tabindex="-1" value=""></div>
+					<input type="submit" value="Subscribe!" name="subscribe" id="mc-embedded-subscribe" class="button subscribe-submit">
+				</form>
+			</div><!--End mc_embed_signup-->
 			</div>
 		</li>
 		<li id="main-menu-search" class="main-menu-item">
@@ -48,7 +58,7 @@
 			<div class="main-menu-dropdown">
 				<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 				<label for="main-menu-search-box">
-					<span hidden class="screen-reader-text"><?php echo _x( 'Search Lawyerist.com: ', 'label' ) ?></span>
+					<span hidden class="screen-reader-text"><?php echo _x( 'Search Lawyerist.com:', 'label' ) ?></span>
 				</label>
 				<input id="main-menu-search-box" type="search" class="search-field" name="s" placeholder="<?php echo esc_attr_x( 'Search Lawyerist.com …', 'placeholder' ) ?>" title="<?php echo esc_attr_x( 'Search Lawyerist.com', 'label' ) ?>" />
 				<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
