f0b51c7ea51d099abf29a0bf3192accbbada6e3e
Sam Glover <sam@samglover.net>
2017-01-02 00:46:02 -0600

    Remove share counts from bylines.

--- postmeta-single.php
+++ postmeta-single.php
@@ -45,28 +45,6 @@ echo '<div class="postmeta">';
 
   }
 
-
-  // Calculate shares.
-
-    /* Twitter (via http://newsharecounts.com/) */
-    $tw_api_call	= file_get_contents( 'http://public.newsharecounts.com/count.json?url=' . $url );
-    $tw_shares		= json_decode( $tw_api_call );
-
-    /* Facebook */
-    // $fb_api_call	= 'http://api.facebook.com/restserver.php?format=json&method=links.getStats&urls=' . urlencode( $url );
-    // $fb_shares		= json_decode( file_get_contents( $fb_api_call ), true );
-
-    /* LinkedIn */
-    $li_api_call	= file_get_contents( 'https://www.linkedin.com/countserv/count/share?url=' . $url . '&format=json' );
-    $li_shares		= json_decode( $li_api_call );
-
-    $shares						= $tw_shares->count /* + $fb_shares[0][share_count] */ + $li_shares->count;
-    $shares_formatted	= number_format( $shares );
-
-  if ( $shares > 10 ) {
-    echo '<span class="shares">' . $shares_formatted . ' Shares </span> ';
-  }
-
   // Comments
   $num_comments	= get_comments_number();
 
