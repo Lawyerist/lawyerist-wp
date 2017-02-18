5766d427bfd860f44972949e7660ed43bf1b94c2
Sam Glover <sam@samglover.net>
2017-02-17 22:10:09 -0600

    Remove Facebook Pixel code.

--- js/footer-scripts.js
+++ js/footer-scripts.js
@@ -11,20 +11,6 @@ jQuery(
 );
 //End Responsive Menu
 
-
-// Facebook Pixel
-!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-document,'script','https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '254894638282349', {
-em: 'insert_email_variable,'
-});
-fbq('track', 'PageView');
-// End Facebook Pixel
-
-
 // MailChimp Goal Tracking
 var $mcGoal = {'settings':{'uuid':'a5da2382c098b6541dcd6cf8e','dc':'us2'}};
 (function() {
