e5158d0c982f808b7dabcfa41f3ae3f9912d5732
Sam Glover <sam@samglover.net>
2017-02-21 15:06:31 -0600

    Add Link post format, set to noindex.

--- head.php
+++ head.php
@@ -24,6 +24,8 @@
 
 <meta name="description" content="<?php echo $description; ?>">
 
+<?php if ( has_post_format( 'link', $post->ID ) ) { echo '<meta name="robots" content="noindex">'; } ?>
+
 <!-- Google Webmaster Tools site verification tag for Sam -->
 <meta name="google-site-verification" content="GwbQ-BLG3G-tXV4-uG-_kZIaxXxm_Wqmzg5wFSBa9hI" />
 <!-- Google Webmaster Tools site verification tag for Aaron -->
