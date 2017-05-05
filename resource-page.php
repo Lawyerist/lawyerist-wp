28d39861bb68daeea4b0e14b4d9f376626322fac
Sam Glover <sam@samglover.net>
2017-05-04 21:56:42 -0500

    Add resource page template.

---
+++ resource-page.php
@@ -0,0 +1,36 @@
+<?php /* Template Name: Resource Page */ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php get_template_part( 'head' ); ?>
+
+<body <?php body_class( $class ); ?>>
+
+<?php get_header(); ?>
+
+<div id="column_container">
+
+	<div id="content_column">
+
+		<?php
+
+		// Get the Loop.
+    get_template_part( 'loop', 'resource-page' );
+
+		?>
+
+	</div><!-- end #content_column -->
+
+	<?php if ( !is_mobile() ) { include('sidebar.php'); } ?>
+
+	<div class="clear"></div>
+
+</div><!--end #column_container-->
+
+<div class="clear"></div>
+
+<?php get_footer(); ?>
+
+</body>
+</html>
