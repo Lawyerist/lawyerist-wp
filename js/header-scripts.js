91da40ddeddc134cad7f6e1cbf68561533f99a9b
Sam Glover <sam@samglover.net>
2017-06-21 10:33:02 -0500

    Ads now refresh every 30 seconds.

--- js/header-scripts.js
+++ js/header-scripts.js
@@ -13,10 +13,18 @@ googletag.cmd = googletag.cmd || [];
 })();
 
 googletag.cmd.push(function() {
-  googletag.defineSlot('/12659965/lawyerist_ap1_leaderboard', [728, 90], 'div-gpt-ad-1429843825352-0').addService(googletag.pubads());
-  googletag.defineSlot('/12659965/lawyerist_ap2_sidebar1', [300, 250], 'div-gpt-ad-1429843825352-1').addService(googletag.pubads());
-  googletag.defineSlot('/12659965/lawyerist_ap3_sidebar2', [300, 250], 'div-gpt-ad-1429843825352-2').addService(googletag.pubads());
+  var ap1 = googletag.defineSlot('/12659965/lawyerist_ap1_leaderboard', [728, 90], 'div-gpt-ad-1429843825352-0').setTargeting("test", "refresh").addService(googletag.pubads());
+  var ap2 = googletag.defineSlot('/12659965/lawyerist_ap2_sidebar1', [300, 250], 'div-gpt-ad-1429843825352-1').setTargeting("test", "refresh").addService(googletag.pubads());
+  var ap3 = googletag.defineSlot('/12659965/lawyerist_ap3_sidebar2', [300, 250], 'div-gpt-ad-1429843825352-2').setTargeting("test", "refresh").addService(googletag.pubads());
+
   googletag.pubads().enableSingleRequest();
+
   googletag.enableServices();
+
+  googletag.display( 'div-gpt-ad-1429843825352-0', 'div-gpt-ad-1429843825352-1', 'div-gpt-ad-1429843825352-2' );
+
+  // Set timer to refresh slot every 30 seconds
+  setInterval( function() { googletag.pubads().refresh( [ ap1, ap2, ap3 ] ); }, 30000 );
+
 });
 // End DoubleClick Tags
