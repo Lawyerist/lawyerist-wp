f008190767f7149138ac8f06879ef85959b5f194
Sam Glover <sam@samglover.net>
2017-11-08 14:36:48 -0600

    Formatting.

--- js/footer-scripts.js
+++ js/footer-scripts.js
@@ -14,7 +14,7 @@ jQuery(
 // MailChimp Goal Tracking
 var $mcGoal = {'settings':{'uuid':'a5da2382c098b6541dcd6cf8e','dc':'us2'}};
 (function() {
-	 var sp = document.createElement('script'); sp.type = 'text/javascript'; sp.async = true; sp.defer = true;
+	var sp = document.createElement('script'); sp.type = 'text/javascript'; sp.async = true; sp.defer = true;
 	sp.src = ('https:' == document.location.protocol ? 'https://s3.amazonaws.com/downloads.mailchimp.com' : 'http://downloads.mailchimp.com') + '/js/goal.min.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sp, s);
 })();
@@ -23,10 +23,11 @@ var $mcGoal = {'settings':{'uuid':'a5da2382c098b6541dcd6cf8e','dc':'us2'}};
 
 // LinkedIn Insight Tag
 _linkedin_data_partner_id = "86294";
-
-(function(){var s = document.getElementsByTagName("script")[0];
-var b = document.createElement("script");
-b.type = "text/javascript";b.async = true;
-b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-s.parentNode.insertBefore(b, s);})();
+(function(){
+  var s = document.getElementsByTagName("script")[0];
+  var b = document.createElement("script");
+  b.type = "text/javascript";b.async = true;
+  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+  s.parentNode.insertBefore(b, s);
+})();
 // End LinkedIn Insight Tag
