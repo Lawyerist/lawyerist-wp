d172866c09f59cda59f022f99b9605f3b892b70c
Sam Glover <sam@samglover.net>
2017-12-11 23:12:34 -0600

    Formatting tweak.

--- page.php
+++ page.php
@@ -11,12 +11,7 @@
 
 	<div id="content_column">
 
-		<?php
-
-		// Get the Loop.
-    get_template_part( 'loop', 'page' );
-
-		?>
+		<?php get_template_part( 'loop', 'page' ); ?>
 
 	</div><!-- end #content_column -->
 
