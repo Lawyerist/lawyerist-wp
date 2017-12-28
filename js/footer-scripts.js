6401647c6732ca7f983a7954282e41f2beb87cbf
Sam Glover <sam@samglover.net>
2017-12-28 09:41:43 -0600

    Remove MailChimp tracking code.

--- js/footer-scripts.js
+++ js/footer-scripts.js
@@ -11,16 +11,6 @@ jQuery(
 // End Responsive Menu
 
 
-// MailChimp Goal Tracking
-var $mcGoal = {'settings':{'uuid':'a5da2382c098b6541dcd6cf8e','dc':'us2'}};
-(function() {
-	var sp = document.createElement('script'); sp.type = 'text/javascript'; sp.async = true; sp.defer = true;
-	sp.src = ('https:' == document.location.protocol ? 'https://s3.amazonaws.com/downloads.mailchimp.com' : 'http://downloads.mailchimp.com') + '/js/goal.min.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sp, s);
-})();
-// End MailChimp Goal Tracking
-
-
 // LinkedIn Insight Tag
 _linkedin_data_partner_id = "86294";
 (function(){
