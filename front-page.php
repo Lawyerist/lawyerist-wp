365823dce1de6f64f51097c18f365f5d4c21e532
Sam Glover <sam@samglover.net>
2018-02-26 15:14:28 -0600

    Saving WIP.

--- front-page.php
+++ front-page.php
@@ -14,6 +14,17 @@
     <?php
 
     // Outputs the Scorecard call to action.
+		echo '<div id="big_hero" class="index_post_container">';
+			echo '<a id="big_hero_top" href="https://lawyerist.com/scorecard/">';
+				echo '<div id="scorecard_image_wrapper"><img src="https://lawyerist.com/lawyerist/wp-content/uploads/2018/02/scorecard.png" alt="The Small Firm Scorecard example" /></div>';
+				echo '<div id="scorecard_prompt_wrapper">';
+					echo '<h2>The Small Firm Scorecard<sup>TM</sup></h2>';
+					echo '<p>Is your law firm structured to succeed in the future?</p>';
+				echo '</div>';
+				echo '<div class="clear"></div>';
+			echo '</a>';
+			echo '<div id="big_hero_button"><a class="button" href="https://lawyerist.com/scorecard/">Get Your Free Score</a></div>';
+		echo '</div>';
 
     // Outputs the Insider, website assessment, and LPJ calls to action.
 
