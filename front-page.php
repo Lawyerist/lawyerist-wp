263d4c208212e54ae55d324e60d141e23f1af713
Sam Glover <sam@samglover.net>
2018-02-27 12:21:54 -0600

    Update scorecard page URL.

--- front-page.php
+++ front-page.php
@@ -16,7 +16,7 @@
     // Outputs the Scorecard call to action.
 		echo '<div id="big_hero" class="index_post_container">';
 			echo '<a id="big_hero_top" href="https://lawyerist.com/scorecard/">';
-				echo '<div id="scorecard_image_wrapper"><img src="https://lawyerist.com/lawyerist/wp-content/uploads/2018/02/scorecard.png" alt="The Small Firm Scorecard example" /></div>';
+				echo '<div id="scorecard_image_wrapper"><img src="https://lawyerist.com/lawyerist/wp-content/uploads/2018/02/scorecard-page.png" alt="The Small Firm Scorecard example" /></div>';
 				echo '<div id="scorecard_prompt_wrapper">';
 					echo '<h2>The Small Firm Scorecard<sup>TM</sup></h2>';
 					echo '<p>Is your law firm structured to succeed in the future?</p>';
