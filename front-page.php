a6e78917f5aaa58fff7a6b606fbba0ef71697ef9
Sam Glover <sam@samglover.net>
2018-03-05 14:54:34 -0600

    Add sticky posts to the front page.

--- front-page.php
+++ front-page.php
@@ -11,6 +11,50 @@
 
 	<div id="content_column">
 
+	<?php
+
+		// Outputs up to 3 sticky posts.
+		$sticky_posts = get_option( 'sticky_posts' );
+
+		$sticky_post_query_args = array(
+			'post__in'						=> $sticky_posts,
+			'posts_per_page'			=> 3,
+		);
+
+		$sticky_post_query = new WP_Query( $sticky_post_query_args );
+
+		if ( $sticky_post_query->have_posts() ) :
+
+			echo '<div id="front_page_sticky_posts">';
+
+			while ( $sticky_post_query->have_posts() ) : $sticky_post_query->the_post();
+
+				$sticky_post_title	= the_title( '', '', FALSE );
+				$sticky_post_url		= get_permalink();
+
+				// Starts the post container.
+				echo '<div ' ;
+				post_class( 'front_page_sticky_post shadow' );
+				echo '>';
+
+					// Starts the link container. Makes for big click targets!
+					echo '<a href="' . $sticky_post_url . '" title="' . $sticky_post_title . '">';
+
+						echo '<h2 class="headline">' . $sticky_post_title . '</h2>';
+
+					echo '</a>';
+
+				echo '</div>';
+
+			endwhile;
+
+			echo '</div>';
+
+		endif;
+
+
+	?>
+
     <!-- Outputs the Scorecard call to action. -->
 		<div id="big_hero_cta" class="index_post_container">
 			<a class="big_hero_top" href="https://lawyerist.com/scorecard/">
@@ -51,205 +95,205 @@
 
 		<div class="front_page_block">
 
-		<?php
+	<?php
 
-			// Outputs the most recent podcast episode.
-			$current_podcast_query_args = array(
-				'category_name'				=> 'lawyerist-podcast',
-				'ignore_sticky_posts' => TRUE,
-				'posts_per_page'			=> 1,
-			);
+		// Outputs the most recent podcast episode.
+		$current_podcast_query_args = array(
+			'category_name'				=> 'lawyerist-podcast',
+			'ignore_sticky_posts' => TRUE,
+			'posts_per_page'			=> 1,
+		);
 
-			$current_podcast_query = new WP_Query( $current_podcast_query_args );
+		$current_podcast_query = new WP_Query( $current_podcast_query_args );
 
-			if ( $current_podcast_query->have_posts() ) : while ( $current_podcast_query->have_posts() ) : $current_podcast_query->the_post();
+		if ( $current_podcast_query->have_posts() ) : while ( $current_podcast_query->have_posts() ) : $current_podcast_query->the_post();
 
-				$podcast_title	= the_title( '', '', FALSE );
-				$podcast_url		= get_permalink();
+			$podcast_title	= the_title( '', '', FALSE );
+			$podcast_url		= get_permalink();
 
-				// Starts the post container.
-				echo '<div ' ;
-				post_class( 'index_post_container has-post-label' );
-				echo '>';
+			// Starts the post container.
+			echo '<div ' ;
+			post_class( 'index_post_container has-post-label' );
+			echo '>';
 
-					// Starts the link container. Makes for big click targets!
-					echo '<a href="' . $podcast_url . '" title="' . $podcast_title . '">';
+				// Starts the link container. Makes for big click targets!
+				echo '<a href="' . $podcast_url . '" title="' . $podcast_title . '">';
 
-						// Now we get the headline and excerpt (except for certain kinds of posts).
-						echo '<div class="headline_excerpt">';
+					// Now we get the headline and excerpt (except for certain kinds of posts).
+					echo '<div class="headline_excerpt">';
 
-							// Outputs the podcast cover image.
-							echo '<div class="default_thumbnail" alt="The Lawyerist Podcast logo" style="background-image: url( https://lawyerist.com/lawyerist-dev/wp-content/uploads/2018/02/lawyerist-ltn-podcast-logo-16x9-684x385.png );"></div>';
+						// Outputs the podcast cover image.
+						echo '<div class="default_thumbnail" alt="The Lawyerist Podcast logo" style="background-image: url( https://lawyerist.com/lawyerist-dev/wp-content/uploads/2018/02/lawyerist-ltn-podcast-logo-16x9-684x385.png );"></div>';
 
-							// Headline
-							echo '<h2 class="headline">' . $podcast_title . '</h2>';
+						// Headline
+						echo '<h2 class="headline">' . $podcast_title . '</h2>';
 
-							// Clearfix
-							echo '<div class="clear"></div>';
+						// Clearfix
+						echo '<div class="clear"></div>';
 
-						echo '</div>'; // Close .headline_excerpt.
+					echo '</div>'; // Close .headline_excerpt.
 
-					echo '</a>'; // This closes the post link container (.post).
+				echo '</a>'; // This closes the post link container (.post).
 
-					// Outputs the label.
-					$cat_IDs = wp_get_post_terms(
-						$post->ID,
-						'category',
-						array(
-							'fields' 	=> 'ids',
-							'orderby' => 'count',
-							'order' 	=> 'DESC'
-						)
-					);
+				// Outputs the label.
+				$cat_IDs = wp_get_post_terms(
+					$post->ID,
+					'category',
+					array(
+						'fields' 	=> 'ids',
+						'orderby' => 'count',
+						'order' 	=> 'DESC'
+					)
+				);
 
-					$cat_info				= get_term( $cat_IDs[0] );
+				$cat_info				= get_term( $cat_IDs[0] );
 
-					$post_label 		   	= $cat_info->name;
-					$post_label_url			=	get_term_link( $cat_IDs[0], 'category' );
+				$post_label 		   	= $cat_info->name;
+				$post_label_url			=	get_term_link( $cat_IDs[0], 'category' );
 
-					if ( !empty( $post_label ) ) {
-						echo '<p class="post_label"><a href="' . $post_label_url . '" title="All episodes of ' . $post_label . '.">All episodes of ' . $post_label . '</a></p>';
-					}
+				if ( !empty( $post_label ) ) {
+					echo '<p class="post_label"><a href="' . $post_label_url . '" title="All episodes of ' . $post_label . '.">All episodes of ' . $post_label . '</a></p>';
+				}
 
-				echo '</div>';
+			echo '</div>';
 
-			endwhile; endif;
-			// End of podcast episode.
-
-
-	    // Outputs the most recent download.
-			$download_query_args = array(
-				'post_type'						=> 'product',
-				'ignore_sticky_posts' => TRUE,
-				'posts_per_page'			=> 1,
-			);
-
-			$download_query = new WP_Query( $download_query_args );
-
-			if ( $download_query->have_posts() ) : while ( $download_query->have_posts() ) : $download_query->the_post();
-
-				$download_title			= the_title( '', '', FALSE );
-				$download_url				= get_permalink();
-				$download_excerpt   = get_the_excerpt();
-			  $seo_descr    		  = get_post_meta( $post->ID, '_yoast_wpseo_metadesc', true );
-
-			  // Sets the post excerpt to the Yoast Meta Description.
-			  if ( !empty( $seo_descr ) ) { $download_excerpt = $seo_descr; }
-
-				// Starts the post container.
-				echo '<div ' ;
-				post_class( 'index_post_container has-post-label' );
-				echo '>';
-
-					// Starts the link container. Makes for big click targets!
-					echo '<a href="' . $download_url . '" title="' . $download_title . '">';
-
-						// Now we get the headline and excerpt (except for certain kinds of posts).
-						echo '<div class="headline_excerpt">';
-
-							// Outputs a featured image.
-							if ( has_post_thumbnail() ) { the_post_thumbnail( 'shop_single' ); }
-
-							// Headline
-							echo '<h2 class="headline">' . $download_title . '</h2>';
-
-							echo '<p class="excerpt">' . $download_excerpt . '</p>';
-
-							echo '<a href="' . $post_url . '" class="button">Get it Now</a>';
-
-							// Clearfix
-							echo '<div class="clear"></div>';
-
-						echo '</div>'; // Close .headline_excerpt.
-
-					echo '</a>'; // This closes the post link container (.post).
-
-					// Outputs the label.
-					echo '<p class="post_label"><a href="https://lawyerist.com/library/" title="Explore the Lawyerist Insider Library.">Explore the Lawyerist Insider Library</a></p>';
-
-				echo '</div>';
-
-			endwhile; endif;
-			// End of download.
+		endwhile; endif;
+		// End of podcast episode.
 
 
-			// Outputs the most recent blog post.
-			$current_post_query_args = array(
-				'category_name'				=> 'blog-posts',
-				'ignore_sticky_posts' => TRUE,
-				'posts_per_page'			=> 1,
-			);
+    // Outputs the most recent download.
+		$download_query_args = array(
+			'post_type'						=> 'product',
+			'ignore_sticky_posts' => TRUE,
+			'posts_per_page'			=> 1,
+		);
 
-			$current_post_query = new WP_Query( $current_post_query_args );
+		$download_query = new WP_Query( $download_query_args );
 
-			if ( $current_post_query->have_posts() ) : while ( $current_post_query->have_posts() ) : $current_post_query->the_post();
+		if ( $download_query->have_posts() ) : while ( $download_query->have_posts() ) : $download_query->the_post();
 
-				$post_title			= the_title( '', '', FALSE );
-				$post_url				= get_permalink();
-				$post_excerpt   = get_the_excerpt();
-			  $seo_descr      = get_post_meta( $post->ID, '_yoast_wpseo_metadesc', true );
+			$download_title			= the_title( '', '', FALSE );
+			$download_url				= get_permalink();
+			$download_excerpt   = get_the_excerpt();
+		  $seo_descr    		  = get_post_meta( $post->ID, '_yoast_wpseo_metadesc', true );
 
-			  // Sets the post excerpt to the Yoast Meta Description.
-			  if ( !empty( $seo_descr ) ) { $post_excerpt = $seo_descr; }
+		  // Sets the post excerpt to the Yoast Meta Description.
+		  if ( !empty( $seo_descr ) ) { $download_excerpt = $seo_descr; }
 
-				// Starts the post container.
-				echo '<div ' ;
-				post_class( 'index_post_container has-post-label' );
-				echo '>';
+			// Starts the post container.
+			echo '<div ' ;
+			post_class( 'index_post_container has-post-label' );
+			echo '>';
 
-					// Starts the link container. Makes for big click targets!
-					echo '<a href="' . $post_url . '" title="' . $post_title . '">';
+				// Starts the link container. Makes for big click targets!
+				echo '<a href="' . $download_url . '" title="' . $download_title . '">';
 
-						// Now we get the headline and excerpt (except for certain kinds of posts).
-						echo '<div class="headline_excerpt">';
+					// Now we get the headline and excerpt (except for certain kinds of posts).
+					echo '<div class="headline_excerpt">';
 
-							// Outputs the featured image.
-							if ( has_post_thumbnail() ) {
-								echo '<div class="default_thumbnail" style="background-image: url( ';
-								echo the_post_thumbnail_url( 'default_thumbnail' );
-								echo ' );"></div>';
-							}
+						// Outputs a featured image.
+						if ( has_post_thumbnail() ) { the_post_thumbnail( 'shop_single' ); }
 
-							// Headline
-							echo '<h2 class="headline">' . $post_title . '</h2>';
+						// Headline
+						echo '<h2 class="headline">' . $download_title . '</h2>';
 
-							echo '<p class="excerpt">' . $post_excerpt . '</p>';
+						echo '<p class="excerpt">' . $download_excerpt . '</p>';
 
-							get_template_part( 'postmeta', 'index' );
+						echo '<a href="' . $post_url . '" class="button">Get it Now</a>';
 
-							// Clearfix
-							echo '<div class="clear"></div>';
+						// Clearfix
+						echo '<div class="clear"></div>';
 
-						echo '</div>'; // Close .headline_excerpt.
+					echo '</div>'; // Close .headline_excerpt.
 
-					echo '</a>'; // This closes the post link container (.post).
+				echo '</a>'; // This closes the post link container (.post).
 
-					// Outputs the label.
-					$cat_IDs = wp_get_post_terms(
-						$post->ID,
-						'category',
-						array(
-							'fields' 	=> 'ids',
-							'orderby' => 'count',
-							'order' 	=> 'DESC'
-						)
-					);
+				// Outputs the label.
+				echo '<p class="post_label"><a href="https://lawyerist.com/library/" title="Explore the Lawyerist Insider Library.">Explore the Lawyerist Insider Library</a></p>';
 
-					$cat_info				= get_term( $cat_IDs[0] );
+			echo '</div>';
 
-					$post_label 		   	= $cat_info->name;
-					$post_label_url			=	get_term_link( $cat_IDs[0], 'category' );
+		endwhile; endif;
+		// End of download.
 
-					if ( !empty( $post_label ) ) {
-						echo '<p class="post_label"><a href="' . $post_label_url . '" title="All ' . $post_label . '.">All ' . $post_label . '</a></p>';
-					}
 
-				echo '</div>';
+		// Outputs the most recent blog post.
+		$current_post_query_args = array(
+			'category_name'				=> 'blog-posts',
+			'ignore_sticky_posts' => TRUE,
+			'posts_per_page'			=> 1,
+		);
 
-			endwhile; endif;
-			// End of blog post.
+		$current_post_query = new WP_Query( $current_post_query_args );
 
-		?>
+		if ( $current_post_query->have_posts() ) : while ( $current_post_query->have_posts() ) : $current_post_query->the_post();
+
+			$post_title			= the_title( '', '', FALSE );
+			$post_url				= get_permalink();
+			$post_excerpt   = get_the_excerpt();
+		  $seo_descr      = get_post_meta( $post->ID, '_yoast_wpseo_metadesc', true );
+
+		  // Sets the post excerpt to the Yoast Meta Description.
+		  if ( !empty( $seo_descr ) ) { $post_excerpt = $seo_descr; }
+
+			// Starts the post container.
+			echo '<div ' ;
+			post_class( 'index_post_container has-post-label' );
+			echo '>';
+
+				// Starts the link container. Makes for big click targets!
+				echo '<a href="' . $post_url . '" title="' . $post_title . '">';
+
+					// Now we get the headline and excerpt (except for certain kinds of posts).
+					echo '<div class="headline_excerpt">';
+
+						// Outputs the featured image.
+						if ( has_post_thumbnail() ) {
+							echo '<div class="default_thumbnail" style="background-image: url( ';
+							echo the_post_thumbnail_url( 'default_thumbnail' );
+							echo ' );"></div>';
+						}
+
+						// Headline
+						echo '<h2 class="headline">' . $post_title . '</h2>';
+
+						echo '<p class="excerpt">' . $post_excerpt . '</p>';
+
+						get_template_part( 'postmeta', 'index' );
+
+						// Clearfix
+						echo '<div class="clear"></div>';
+
+					echo '</div>'; // Close .headline_excerpt.
+
+				echo '</a>'; // This closes the post link container (.post).
+
+				// Outputs the label.
+				$cat_IDs = wp_get_post_terms(
+					$post->ID,
+					'category',
+					array(
+						'fields' 	=> 'ids',
+						'orderby' => 'count',
+						'order' 	=> 'DESC'
+					)
+				);
+
+				$cat_info				= get_term( $cat_IDs[0] );
+
+				$post_label 		   	= $cat_info->name;
+				$post_label_url			=	get_term_link( $cat_IDs[0], 'category' );
+
+				if ( !empty( $post_label ) ) {
+					echo '<p class="post_label"><a href="' . $post_label_url . '" title="All ' . $post_label . '.">All ' . $post_label . '</a></p>';
+				}
+
+			echo '</div>';
+
+		endwhile; endif;
+		// End of blog post.
+
+	?>
 
 	</div> <!-- End of .front_page_block with the current podcast, download, and blog post. -->
 
