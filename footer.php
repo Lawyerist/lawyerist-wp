63f93730c7afb002a96eaf33635b2afa8ad343be
Sam Glover <sam@samglover.net>
2018-03-06 09:05:26 -0600

    Update footer.

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 
 		<div id="footer_legal">
 			<p>The original content within this website is &copy; <?php echo date('Y') ?>.</p>
-			<p>LAWYERIST, LAWYERIST LAB, and LAWYERIST SITES are trademarks registered by Lawyerist Media, LLC.</p>
+			<p>Lawyerist, Lawyerist Lab, TBD Law, and The Small Firm Scorecard are trademarks registered by Lawyerist Media, LLC.</p>
 			<p><a href="<?php echo home_url(); ?>/privacy-policy/">Privacy policy</a> / <a href="<?php echo home_url(); ?>/ftc-disclosures/">FTC disclosures</a> / <a href="<?php echo home_url(); ?>/sitemap_index.xml">XML sitemap</a></p>
 	  </div>
 
