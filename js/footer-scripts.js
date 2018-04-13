c1428e132bd4e34a6e07c330dc8db3314bb0da2f
Sam Glover <sam@samglover.net>
2018-04-13 10:36:38 -0500

    Update LinkedIn Insight tag.

--- js/footer-scripts.js
+++ js/footer-scripts.js
@@ -60,15 +60,3 @@ jQuery(
   }
 );
 // End Sticky Sidebar Ad
-
-
-// LinkedIn Insight Tag
-_linkedin_data_partner_id = "86294";
-(function(){
-  var s = document.getElementsByTagName("script")[0];
-  var b = document.createElement("script");
-  b.type = "text/javascript";b.async = true;
-  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-  s.parentNode.insertBefore(b, s);
-})();
-// End LinkedIn Insight Tag
