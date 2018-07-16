0b51dd6d05fa583bdfec16dc1b60909fffa1fbfb
Sam Glover <sam@samglover.net>
2018-07-16 16:16:24 -0500

    Change single-post header style for community posts.

--- loop-single.php
+++ loop-single.php
@@ -38,20 +38,31 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 
       }
 
+      // Shows author avatar on community posts.
+      if ( has_category( 'community-posts' ) ) {
+        $author_name   = get_the_author_meta( 'display_name' );
+        $author_avatar = get_avatar( get_the_author_meta( 'user_email' ), 100, '', $author_name );
+
+        echo '<div class="author_avatar">';
+        echo $author_avatar;
+        echo '</div>';
+      }
+
       // Headline
       echo '<h1 class="headline entry-title">' . $post_title . '</h1>';
 
       // Call the lawyerist_postmeta function, which outputs the byline, date,
-      // and share and comment counts.
+      // and comment count.
       get_template_part( 'postmeta', 'single_top' );
 
     echo '</div>'; // Close .headline_postmeta.
 
-    // Show featured image (1) if the post has a featured image AND (2) if it's
-    // the first page of the post AND (3) the post DOES NOT have the no-image tag.
-    if ( has_post_thumbnail() && !has_tag('no-image') ) { the_post_thumbnail('standard_thumbnail'); }
+    // Shows featured image if (1) this isn't a community post AND (2) this post
+    // has a featured image AND (3) it is the first page of the post AND (4) the
+    // post DOES NOT have the no-image tag.
+    if ( !has_category( 'community-posts' ) && has_post_thumbnail() && !has_tag('no-image') ) { the_post_thumbnail('standard_thumbnail'); }
 
-    // If the post is in a series, link to the series.
+    // If the post is in a series, shows a link to the series.
     if ( has_term( true , 'series' ) ) {
 
       // Uses variables from earlier in the loop, when we put the series title
@@ -128,7 +139,10 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
 
     echo '</div>'; // Close .post_body.
 
-  	lawyerist_get_author_bio();
+    // Outputs the author bio except on community posts.
+  	if ( !has_category( 'community-posts' ) ) {
+      lawyerist_get_author_bio();
+    }
 
     scorecard_cta();
 
