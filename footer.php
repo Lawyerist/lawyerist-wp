8a94bf1b9ce7c9d997b65d2f549ff07f783d98b5
Sam Glover <sam@samglover.net>
2018-10-02 10:57:25 -0500

    LawyeristBot

--- footer.php
+++ footer.php
@@ -34,3 +34,10 @@ s.parentNode.insertBefore(b, s);})();
 <!-- Amazon Associates OneTag -->
 <div id="amzn-assoc-ad-01e6e315-1d4b-424c-8d56-fc2b9f9463bb"></div><script async src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=01e6e315-1d4b-424c-8d56-fc2b9f9463bb"></script>
 <!-- End Amazon Associates OneTag -->
+
+<!-- LawyeristBot -->
+<link href="https://lawdroid.com/ld-assets/webchat.css?ver=1.1.7" rel="stylesheet" type="text/css">
+<script src="https://lawdroid.com/ld-assets/webchat.js?ver=1.1.7"></script>
+<script>LD_Widget_Init('botid=91944&color=c00&color_bot=dadada&token=aa0f6263eb13149dfe6d9fcc25cda6fc&display_avatars=true&avatar_bot=https://lawdroid.com/wp-content/uploads/2018/08/Lawyerist-Logo.png&avatar_human=https://lawdroid.com/wp-content/uploads/2018/08/user-avatar-shadowed.jpg&sendBtn=Send&inputBox=Type%20something...&bottom_margin=10&user_input_bg_color=000000&user_input_field_focus_color=c00&chatbot_id=1727',true, '600px','100%','https://lawdroid.com/wp-content/uploads/2018/08/Lawyerist-Logo.png','https://lawdroid.com/privacy_policy',0,false,true,'Hi%20there%21%20%uD83D%uDC4B%20Thanks%20for%20visiting%20Lawyerist.%20Would%20you%20like%20me%20to%20show%20you%20around%3F%20Try%20me.','250','90','LawyeristBot', 'c00');
+</script>
+<!-- End LawyeristBot -->
