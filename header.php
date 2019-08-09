0bae858efe822b853e2db58197c15751ee360973
Sam Glover <sam@samglover.net>
2019-08-09 14:13:18 -0500

    Remove duplicate Pinterest site verification.

--- header.php
+++ header.php
@@ -3,6 +3,14 @@
 
 <head>
 
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-WVFM84N');</script>
+  <!-- End Google Tag Manager -->
+
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
@@ -37,7 +45,6 @@
    window.dataLayer = window.dataLayer || [];
    function gtag(){dataLayer.push(arguments);}
    gtag('js', new Date());
-
    gtag('config', 'AW-928946623');
   </script>
   <!-- End Google global site tag. -->
@@ -69,12 +76,14 @@
   <!-- Google Webmaster Tools site verification tag for Aaron -->
   <meta name="google-site-verification" content="d_OrAi2nt_o3Y3uQ-dicRpRYaxZSynFLUhHY15cnJUY" />
 
-  <!-- Pinterest site verification. -->
-  <meta name="p:domain_verify" content="53c0a3083959448fb93c9672226a472e"/>
-
 </head>
 
 <body <?php body_class(); ?>>
+
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WVFM84N"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 
   <div id="header-grid">
 
