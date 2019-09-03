143cd441cd8c0d710ba374e738f66037340684d8
Sam Glover <samglover@MBP-Sam.local>
2019-09-03 15:34:08 -0500

    Add address & phone number to site footer.

--- footer.php
+++ footer.php
@@ -2,10 +2,18 @@
 
 		<div id="footer">
 
+			<p>Lawyerist.com<br />331 2nd Avenue South<br />Minneapolis, MN 55401</p>
+
+			<p>612-424-9855</p>
+
 			<div id="footer_legal">
+
 				<p>The original content within this website is &copy; <?php echo date('Y') ?>.</p>
+
 				<p>Lawyerist, Lawyerist Lab, TBD Law, and The Small Firm Scorecard are trademarks registered by Lawyerist Media, LLC.</p>
+
 				<p><a href="<?php echo home_url(); ?>/privacy-policy/">Privacy policy</a> / <a href="<?php echo home_url(); ?>/sitemap_index.xml">XML sitemap</a></p>
+
 		  </div>
 
 			<?php if ( is_singular() ) { echo '<p id="pageID">Page ID: ' . $post->ID . '</p>'; } ?>
