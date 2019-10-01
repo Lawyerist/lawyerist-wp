51518becb3424ef786312b730e6656dbc9dcbe1a
Sam Glover <sam@samglover.net>
2019-10-01 13:38:13 -0500

    Prevent community menu from flashing when clicked.

--- js/footer-scripts.js
+++ js/footer-scripts.js
@@ -12,7 +12,7 @@
   // Opens and closes second-level+ sub-menus.
   // The .not in this function excludes the Products & Services and
   // Join the Lawyerist Community sub menus.
-  $( "#menu-main-menu > .menu-item-has-children .menu-item-has-children > a" ).not( "#menu-item-305888 > a", "#menu-item-270912 > a" ).click( function() {
+  $( "#menu-main-menu > .menu-item-has-children .menu-item-has-children > a" ).not( "#menu-item-305888 > a, #menu-item-270912 > a" ).click( function() {
     $( this ).toggleClass( "open" ).next( ".sub-menu" ).slideToggle( 95 );
   });
 
