17941dc0760b00f5ae81c3b00a5ab7e3f785b3e9
Sam Glover <sam@samglover.net>
2019-11-05 09:19:43 -0600

    Remove Google Tag Manager.

--- header.php
+++ header.php
@@ -3,13 +3,6 @@
 
 <head>
 
-  <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-WVFM84N');</script>
-
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
@@ -131,11 +124,6 @@
 </head>
 
 <body <?php body_class(); ?>>
-
-  <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WVFM84N"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <!-- End Google Tag Manager (noscript) -->
 
 	<?php	echo get_lawyerist_login(); ?>
 
