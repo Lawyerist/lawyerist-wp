2f1b12b309bd84f4d33714f473040882cd28762b
Sam Glover <sam@samglover.net>
2019-11-05 09:39:18 -0600

    Remove superfluous ad delivery code.

--- header.php
+++ header.php
@@ -74,16 +74,6 @@
 
   ?>
 
-  <!-- Google global site tag (gtag.js) - Google Ads: 928946623 -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-928946623"></script>
-  <script>
-   window.dataLayer = window.dataLayer || [];
-   function gtag(){dataLayer.push(arguments);}
-   gtag('js', new Date());
-   gtag('config', 'AW-928946623');
-  </script>
-  <!-- End Google global site tag. -->
-
   <!-- DoubleClick Script -->
   <script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
   <script>
@@ -103,17 +93,6 @@
   });
   </script>
   <!-- End DoubleClick Tag for Sidebar Ad -->
-
-  <!-- Google Ads code for sidebar display ad placement. -->
-  <script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
-  <script>
-    window.googletag = window.googletag || {cmd: []};
-    googletag.cmd.push(function() {
-      googletag.defineSlot('/12659965/lawyerist_300x250_ad_position', [300, 250], 'div-gpt-ad-1565383693580-0').addService(googletag.pubads());
-      googletag.pubads().enableSingleRequest();
-      googletag.enableServices();
-    });
-  </script>
 
   <!-- Google Webmaster Tools site verification tag for Sam -->
   <meta name="google-site-verification" content="GwbQ-BLG3G-tXV4-uG-_kZIaxXxm_Wqmzg5wFSBa9hI" />
