7fa5bcd69be3de24b75d6ec6c2782a2ae9385020
Sam Glover <samglover@MBP-Sam.local>
2020-01-03 12:53:10 -0600

    Remove contact information from the footer.

--- footer.php
+++ footer.php
@@ -2,19 +2,11 @@
 
 		<div id="footer">
 
-			<p>Lawyerist Media, LLC<br />331 2nd Avenue South<br />Minneapolis, MN 55401</p>
+			<p>The original content within this website is &copy; <?php echo date('Y') ?>.</p>
 
-			<p>612-424-9855</p>
+			<p>Lawyerist, Lawyerist Lab, TBD Law, Small Firm Dashboard, and The Small Firm Scorecard are trademarks registered by Lawyerist Media, LLC.</p>
 
-			<div id="footer_legal">
-
-				<p>The original content within this website is &copy; <?php echo date('Y') ?>.</p>
-
-				<p>Lawyerist, Lawyerist Lab, TBD Law, Small Firm Dashboard, and The Small Firm Scorecard are trademarks registered by Lawyerist Media, LLC.</p>
-
-				<p><a href="<?php echo home_url(); ?>/privacy-policy/">Privacy policy</a> / <a href="<?php echo home_url(); ?>/sitemap_index.xml">XML sitemap</a></p>
-
-		  </div>
+			<p><a href="<?php echo home_url(); ?>/privacy-policy/">Privacy policy</a> / <a href="<?php echo home_url(); ?>/sitemap_index.xml">XML sitemap</a></p>
 
 			<?php if ( is_singular() ) { echo '<p id="pageID">Page ID: ' . $post->ID . '</p>'; } ?>
 
